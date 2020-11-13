4e1d2a7b06c6f967ae2fff111a75529a81b361cf
Mooponrawit <68629338+Mooponrawit@users.noreply.github.com>
2020-11-13 12:28:50 +0900

    js

--- sample/js/script05.js
+++ sample/js/script05.js
@@ -1,20 +1,24 @@
-
 $(function(){
 
-    $("#intro p").hide().fadeIn(1000)
-    setTimeout(introAnime,2000);//2秒経った時にintroAnimeを実行
-    //$("#intro").on("click",introAnime)//クリックしたらintroAnimeを実行
-  
-    function introAnime(){//jqueryでフェードアウト、終わったらcontentAnime実行
-      $("#intro").fadeOut(1000);
-    }
-  
-      $("#slide").slick({dots:true});
-    
-      // $(".btn").on("click",()=>{
-      //   $("#slide").slideToggle();
-      // })
+  $("#intro p").hide().fadeIn(1000)
+  setTimeout(introAnime,2000);//2秒経った時にintroAnimeを実行
+  //$("#intro").on("click",introAnime)//クリックしたらintroAnimeを実行
+
+  function introAnime(){//jqueryでフェードアウト、終わったらcontentAnime実行
+    $("#intro").fadeOut(1000);
     
     
+    
+  }
+
+    $("#slide").slick({dots:true});
   
-  })
+    // $(".btn").on("click",()=>{
+    //   $("#slide").slideToggle();
+    // })
+
+    
+    
+  
+
+})
